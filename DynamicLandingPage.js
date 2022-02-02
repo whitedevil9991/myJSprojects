7106488cfee16a744dfdff953a6b21dd0ed57350
whitedevil9991 <revanth.pilli@yahoo.com>
2022-02-03 00:06:17 +0530

    master

---
+++ DynamicLandingPage.js
@@ -0,0 +1,7 @@
+// DOM elements
+const time = document.getElementById('time'),
+greeting=document.getElementById('greeting'),
+name=document.getElementById('name'),
+focus=document.getElementById('focus');
+
+// a function to show the time
