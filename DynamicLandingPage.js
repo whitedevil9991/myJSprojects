58d9f048aaa7cf5e8908d3dc14b035cc6fab8b2a
whitedevil9991 <revanth.pilli@yahoo.com>
2022-02-03 11:38:25 +0530

    master

--- DynamicLandingPage.js
+++ DynamicLandingPage.js
@@ -5,7 +5,7 @@ name=document.getElementById('name'),
 focus=document.getElementById('focus');
 
 // a function to show the time
-function showtime() {
+function ShowTime() {
     let today = new Date(),
     hour = today.getHours(),
     min = today.getMinutes(),
@@ -18,13 +18,41 @@ function showtime() {
     hour = hour%12 | 12 ;
 
     //output the time
-    time.innerHTML='${hour}<span>:</span>${min}<span>:</span>${sec}';
+    time.innerHTML='${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}';
 
 
     /*takes in function showtime and when do we want to call it, like in interval
     here we set the interval to 1000, which means 1000 milliseconds*/
-    setTimeout(showtime, 1000);
+    setTimeout(ShowTime, 1000);
+}
+
+//for adding zeros
+function addZero(){
+    return(parseInt(n,10)<10 ? '0' : '')+n;
+}
+
+//set background according to the time
+function setbg(){
+    let today = new Date();
+    hour= today.getHours();
+
+    if(hour>12) {
+        //morning
+        document.body.style.backgroundImage="";
+        greeting.textContent='good morning';
+    }
+    else if(hour<18){
+//afternoon
+document.body.style.backgroundImage="";
+greeting.textContent='good afternoon';
+    }
+    else{
+        //evening
+        document.body.style.backgroundImage="";
+        greeting.textContent='good evening';
+        document.body.style.color='white';
+    }
 }
 
 //run
-showtime();
+ShowTime();
